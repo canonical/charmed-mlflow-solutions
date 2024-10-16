6e50a31b66dc6173aed02722ab9bde04c9e05de8
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-10-16 10:32:32 +0300

    feat: Add mlflow-server logging relation

--- modules/mlflow/cos_configuration.tf
+++ modules/mlflow/cos_configuration.tf
@@ -91,6 +91,21 @@ resource "juju_integration" "mlflow_server_grafana_agent_k8s_metrics_endpoint" {
   }
 }
 
+resource "juju_integration" "mlflow_server_grafana_agent_k8s_logging" {
+  count = var.cos_configuration ? 1 : 0
+  model = var.create_model ? juju_model.kubeflow[0].name : var.model
+
+  application {
+    name     = module.mlflow_server.app_name
+    endpoint = module.mlflow_server.requires.logging
+  }
+
+  application {
+    name     = var.existing_grafana_agent_name == null ? juju_application.grafana-agent-k8s-mlflow[count.index].name : var.existing_grafana_agent_name
+    endpoint = "logging-provider"
+  }
+}
+
 resource "juju_integration" "minio_grafana_agent_k8s_grafana_dashboard" {
   count = var.cos_configuration ? 1 : 0
   model = var.create_model ? juju_model.kubeflow[0].name : var.model
