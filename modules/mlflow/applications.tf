049d76330732c40f2da3f87f95f8d1fee845d172
NohaIhab <noha.ihab@canonical.com>
2025-08-01 13:09:35 +0300

    [DO NOT MERGE] test: use mysql-k8s 8.0/edge

--- modules/mlflow/applications.tf
+++ modules/mlflow/applications.tf
@@ -23,7 +23,7 @@ module "mlflow_mysql" {
   source          = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=main"
   juju_model_name = var.create_model ? juju_model.kubeflow[0].name : var.model
   app_name        = "mlflow-mysql"
-  channel         = "8.0/stable"
+  channel         = "8.0/edge"
   # The following config is equivalent to "constraints: mem=2G"
   config = {
     profile-limit-memory = "2048"
