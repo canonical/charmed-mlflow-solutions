7661136a96b67386d53f330d65a56f5993f730c4
Tevfik Oguzhan Aksoy <tevfik.aksoy@canonical.com>
2025-10-07 09:37:12 +0300

    fix: change juju_model_name to model_name to adhere to changes in mysql-k8s-operator

--- modules/mlflow/applications.tf
+++ modules/mlflow/applications.tf
@@ -30,10 +30,10 @@ module "mlflow_server" {
 
 module "mlflow_mysql" {
   # tflint-ignore: terraform_module_pinned_source
-  source          = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=main"
-  juju_model_name = var.create_model ? juju_model.kubeflow[0].name : var.model
-  app_name        = "mlflow-mysql"
-  channel         = "8.0/stable"
+  source     = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=main"
+  model_name = var.create_model ? juju_model.kubeflow[0].name : var.model
+  app_name   = "mlflow-mysql"
+  channel    = "8.0/stable"
   # The following config is equivalent to "constraints: mem=2G"
   config = {
     profile-limit-memory = "2048"
