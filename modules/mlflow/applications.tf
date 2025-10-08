757ab49fbec42a7dec122b4a5626187ac53a5b33
deusebio <enrico.deusebio@canonical.com>
2025-10-08 18:37:01 +0200

    chore: updating mysql tf reference (#24) (#23)

--- modules/mlflow/applications.tf
+++ modules/mlflow/applications.tf
@@ -30,7 +30,7 @@ module "mlflow_server" {
 
 module "mlflow_mysql" {
   # tflint-ignore: terraform_module_pinned_source
-  source     = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=93d6608632ae791bffce18e3b74816ebe26d1c5a"
+  source     = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=eb6261e6fd1830d80aa4fa260d091c9110c24ba4"
   model_name = var.create_model ? juju_model.kubeflow[0].name : var.model
   app_name   = "mlflow-mysql"
   channel    = "8.0/stable"
