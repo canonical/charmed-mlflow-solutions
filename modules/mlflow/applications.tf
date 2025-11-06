0a0b984971aaff2b42f1c91ce14235e2f540c347
Enrico Deusebio <enrico.deusebio@canonical.com>
2025-11-06 18:53:47 +0100

    [MISC] Freeze status at 2025-07-30T17:57:02.802480Z

--- modules/mlflow/applications.tf
+++ modules/mlflow/applications.tf
@@ -1,6 +1,6 @@
 module "minio" {
   app_name   = "mlflow-minio"
-  source     = "git::https://github.com/canonical/minio-operator//terraform?ref=track/ckf-1.9"
+  source     = "git::https://github.com/canonical/minio-operator//terraform?ref=9c1d621124df098c24e00329c0d2e1b12b9bc4fa"
   model_name = var.create_model ? juju_model.kubeflow[0].name : var.model
   revision   = var.mlflow_minio_revision
   storage_directives = {
@@ -9,7 +9,7 @@ module "minio" {
 }
 
 module "mlflow_server" {
-  source     = "git::https://github.com/canonical/mlflow-operator//terraform?ref=track/2.15"
+  source     = "git::https://github.com/canonical/mlflow-operator//terraform?ref=6c2cd2e5cca8b04b26b1e3b886f220d9a3de3963"
   model_name = var.create_model ? juju_model.kubeflow[0].name : var.model
   config = {
     enable_mlflow_nodeport = var.enable_mlflow_nodeport,
@@ -20,7 +20,7 @@ module "mlflow_server" {
 
 module "mlflow_mysql" {
   # tflint-ignore: terraform_module_pinned_source
-  source          = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=main"
+  source          = "git::https://github.com/canonical/mysql-k8s-operator//terraform?ref=4f198a8a2787a25da89c1240b3bf7efd1a7d1228"
   juju_model_name = var.create_model ? juju_model.kubeflow[0].name : var.model
   app_name        = "mlflow-mysql"
   channel         = "8.0/stable"
